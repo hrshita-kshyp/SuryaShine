563b5db345368b93cfaf810b268c92dba1208443
Harshita <85073963+hrshita-kshyp@users.noreply.github.com>
2025-06-24 16:50:21 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/home';
+import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Industries from './pages/Industries';
